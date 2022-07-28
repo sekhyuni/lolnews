0b5bb0bb4ee2faa030d188ac0904b6e9f2aba5d1
sekhyuni <sekhyuni@gmail.com>
2022-07-29 00:36:38 +0900

    backend api index.js 파일 내 mongoose 코드 제거

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const router = express.Router();
 
 app.use(express.json());
 
-app.listen(8080, () => {
+app.listen(31053, () => {
     console.log('Server is started');
 });
-
-// app.post('/accounts/login', (req, res) => {
-//     const { id, password } = req.body;
-
-//     fs.readFile('./accounts.json', (err, data) => {
-//         const accounts = JSON.parse(data);
-
-//         let accountFound = false;
-
-//         accounts.forEach(account => {
-//             if (account.id === id && account.password === password) {
-//                 accountFound = true;
-//                 res.send('Success!');
-//             }
-//         });
-
-//         if (!accountFound) {
-//             res.status(406).send('Error!');
-//         }
-//     });
-// });
 
 app.get('/search/keyword', (req, res) => {
     const query = req.query;
@@ -40,10 +15,10 @@ app.get('/search/keyword', (req, res) => {
     res.send(query);
 });
 
-// app.post('/search/keyword', (req, res) => {
-//     const body = req.body;
+app.post('/search/keyword', (req, res) => {
+    const body = req.body;
 
-//     console.log(body);
+    console.log(body);
 
-//     res.send(body);
-// });
+    res.send(body);
+});
