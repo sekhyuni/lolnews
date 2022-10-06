db37739acac65cc35cb668f45b428a621a88ad93
sekhyuni <sekhyuni@gmail.com>
2022-10-06 21:34:25 +0900

    fix: 워드클라우드 API 서버 port 추가로 인한 기존 Back-End API 미연동 오류 수정

--- frontend/src/functions/doAxiosRequest.tsx
+++ frontend/src/functions/doAxiosRequest.tsx
@@ -1,15 +1,29 @@
-import axios from 'axios';
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://172.24.24.84:31053' : '';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const doAxiosRequest = (method: string, url: string, params?: any, isModelAPICall: boolean = false): any => {
-    if (isModelAPICall) {
-        axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://172.24.24.84:31707' : '';
+const NORMAL_PORT: number = 31053;
+const MODEL_PORT: number = 31707;
+
+class ServerPort {
+    port: number;
+
+    constructor(port: number) {
+        this.port = port;
     }
+}
 
+const listOfServerPort: Array<ServerPort> = [new ServerPort(NORMAL_PORT), new ServerPort(MODEL_PORT),];
+
+const [axiosInstanceForNormalAPICall, axiosInstanceForModelAPICall] = listOfServerPort.map((serverPort: ServerPort): AxiosInstance => {
+    return axios.create({
+        baseURL: process.env.NODE_ENV === 'production' ? `http://172.24.24.84:${serverPort.port}` : ''
+    });
+});
+
+const doAxiosRequest = (method: string, url: string, params?: any, isModelAPICall: boolean = false): Promise<AxiosResponse> | undefined => {
     if (method === 'GET') {
-        return axios.get(url, { params, });
+        return isModelAPICall ? axiosInstanceForModelAPICall.get(url, { params, }) : axiosInstanceForNormalAPICall.get(url, { params, });
     } else if (method === 'POST') {
-        return axios.post(url, params);
+        return isModelAPICall ? axiosInstanceForModelAPICall.post(url, params) : axiosInstanceForNormalAPICall.post(url, params);
     }
 };
 
