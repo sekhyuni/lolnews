a91bcddfcf3f71655abe4e2c2991fafc6ee21e03
sekhyuni <sekhyuni@gmail.com>
2022-10-10 17:12:37 +0900

    Revert "feat: 초기 페이지 로딩 속도 개선 (code splitting : React.lazy 함수를 통한 각종 페이지 동적 import)"
    This reverts commit 033559d400ed3c814faa0521a81fb0c79f1b06ca.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Main from './pages/main/Main';
-// import Join from './pages/join/Join';
-// import Login from './pages/login/Login';
-// import Search from './pages/search/index';
-// import Notfound from './pages/notfound/Notfound';
-
-const Main = lazy(() => import('./pages/main/Main'));
-const Join = lazy(() => import('./pages/join/Join'));
-const Login = lazy(() => import('./pages/login/Login'));
-const Search = lazy(() => import('./pages/search/index'));
-const Notfound = lazy(() => import('./pages/notfound/Notfound'));
+import Main from './pages/main/Main';
+import Join from './pages/join/Join';
+import Login from './pages/login/Login';
+import Search from './pages/search/index';
+import Notfound from './pages/notfound/Notfound';
 
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
@@ -26,15 +19,13 @@ declare module 'react' {
 const App = () => {
   return (
     <Router>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/search/*" element={<Search />} />
-          <Route path="/*" element={<Notfound />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search/*" element={<Search />} />
+        <Route path="/*" element={<Notfound />} />
+      </Routes>
     </Router>
   );
 };
