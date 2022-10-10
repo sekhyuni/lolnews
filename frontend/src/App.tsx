9638f584a707ebd1ed13f0965f6049e5725ef724
sekhyuni <sekhyuni@gmail.com>
2022-10-10 17:13:58 +0900

    feat: 초기 페이지 로딩 속도 개선 (code splitting : React.lazy 함수를 통한 각종 페이지 동적 import)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './pages/main/Main';
-import Join from './pages/join/Join';
-import Login from './pages/login/Login';
-import Search from './pages/search/index';
-import Notfound from './pages/notfound/Notfound';
+// import Main from './pages/main/Main';
+// import Join from './pages/join/Join';
+// import Login from './pages/login/Login';
+// import Search from './pages/search/index';
+// import Notfound from './pages/notfound/Notfound';
+
+const Main = lazy(() => import('./pages/main/Main'));
+const Join = lazy(() => import('./pages/join/Join'));
+const Login = lazy(() => import('./pages/login/Login'));
+const Search = lazy(() => import('./pages/search/index'));
+const Notfound = lazy(() => import('./pages/notfound/Notfound'));
 
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
@@ -19,13 +26,15 @@ declare module 'react' {
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/search/*" element={<Search />} />
-        <Route path="/*" element={<Notfound />} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search/*" element={<Search />} />
+          <Route path="/*" element={<Notfound />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
