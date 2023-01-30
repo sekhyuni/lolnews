e9d6e107622c3d03d900637a92e666d5ced740d6
김석현 <sekhyuni@gmail.com>
2023-01-30 09:49:03 +0900

    frontend, backend 디렉토리 제외 모두 제거

--- fastapi/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-class es_config():
-    base_path = '/root/elasticsearch/'
-    es_address = ['https://elastic:4w88klz48xcgvdq8m9gsncvt@192.168.1.10:32311']
-    index_name = 'news_index'
-    csv_data = "2022-07-13.csv"
